ace43d5a87b632b523f90372d857ae9b6fcc4c97
RyanGormican <ryan.gormican@gmail.com>
2023-11-13 02:28:48 -0600

    a
    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef  } from 'react';
 import { Icon } from '@iconify/react';
 import Button from '@mui/material/Button';
+
 function shuffleArray(array) {
   let shuffledArray = array.slice();
   for (let i = shuffledArray.length - 1; i > 0; i--) {
@@ -18,12 +19,17 @@ function App() {
   const [incorrectAnswers, setIncorrectAnswers] = useState(0);
   const [answerStreak, setAnswerStreak] = useState(0);
   const [userAnswer, setUserAnswer] = useState(null);
-  const [timer,setTimer]=useState(15);
+  const [timer, setTimer] = useState(15);
   const [bestAnswerStreak, setBestAnswerStreak] = useState(0);
   const [questionTags, setQuestionTags] = useState([]);
-  const [view,setView] = useState('basic');
+  const [tagStats, setTagStats] = useState({});
+  const [view, setView] = useState('basic');
   const [statsView, setStatsView] = useState('total');
-      const fetchQuestion = () => {
+  const fetchingQuestion = useRef(false);
+   const fetchQuestion = () => {
+    if (!fetchingQuestion.current) {
+      fetchingQuestion.current = true;
+
       fetch('https://the-trivia-api.com/v2/questions/')
         .then(response => response.json())
         .then(data => {
@@ -32,65 +38,102 @@ function App() {
             answers: shuffleArray([...data[0].incorrectAnswers, data[0].correctAnswer]),
           });
         })
-        .catch(error => console.error('Error fetching question:', error));
-    };
+        .catch(error => console.error('Error fetching question:', error))
+        .finally(() => {
+          fetchingQuestion.current = false;
+        });
+    }
+  };
+
   useEffect(() => {
-//  fetchQuestion();
+    fetchQuestion();
   }, []);
+
   useEffect(() => {
-  if (question) {
-    setQuestionTags(prevTags => [...prevTags, ...question.tags]);
-  }
-}, [question]);
-   useEffect(() => {
-  const interval = setInterval(() => {
-  if (userAnswer === null)
-  {
-    setTimer(prevTimer => prevTimer - 1);
-  }
-  }, 1000);
 
-  if (timer === 0 && userAnswer === null) {
-    clearInterval(interval);
-    setUserAnswer('show');
+    if (question) {
+      setQuestionTags(prevTags => [...prevTags, ...question.tags]);
+    }
+  }, [question]);
 
-    setTimeout(() => {
-      setIncorrectAnswers(incorrectAnswers+1);
-      setUserAnswer(null);
- //    fetchQuestion();
-      setTimer(15);
-    }, 3000);
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (userAnswer === null) {
+        setTimer(prevTimer => prevTimer - 1);
+      }
+    }, 1000);
 
-  return () => clearInterval(interval);
-}, [timer, userAnswer]);
-
-  const handleAnswer = (Answer) => {
-    if (userAnswer === null) {
-      setUserAnswer(Answer);
+    if (timer === 0 && userAnswer === null) {
+      clearInterval(interval);
+      setUserAnswer('show');
 
       setTimeout(() => {
-        if (Answer === question.correctAnswer) {
-          setAnswerStreak(answerStreak + 1);
-          setCorrectAnswers(correctAnswers + 1);
-               if (answerStreak+1 > bestAnswerStreak)
-          {
-          setBestAnswerStreak(answerStreak+1);
-          }
-        } else {
-          setAnswerStreak(0);
-          setIncorrectAnswers(incorrectAnswers + 1);
-        }
-         if (answerStreak > bestAnswerStreak)
-          {
-          setBestAnswerStreak(answerStreak);
-          }
-  //      fetchQuestion();
+        setIncorrectAnswers(incorrectAnswers + 1);
+        question.tags.forEach(tag => {
+          setTagStats(prevStats => {
+            const newStats = { ...prevStats };
+            if (newStats[tag]) {
+              newStats[tag].total += 1;
+            } else {
+              newStats[tag] = { total: 1, correct: 0 };
+            }
+            return newStats;
+          });
+        });
         setUserAnswer(null);
+        fetchQuestion();
         setTimer(15);
       }, 3000);
     }
-  };
+
+    return () => clearInterval(interval);
+  }, [timer, userAnswer]);
+
+const handleAnswer = (Answer) => {
+  if (userAnswer === null) {
+    setUserAnswer(Answer);
+
+    setTimeout(() => {
+      question.tags.forEach(tag => {
+        setTagStats(prevStats => {
+          const newStats = { ...prevStats };
+          if (newStats[tag]) {
+            console.log(tag);
+            newStats[tag] = prevState => ({
+              total: prevState.total + 1,
+              correct: Answer === question.correctAnswer ? prevState.correct + 1 : prevState.correct,
+            });
+          } else {
+            newStats[tag] = {
+              total: 1,
+              correct: Answer === question.correctAnswer ? 1 : 0,
+            };
+            console.log(tag);
+          }
+          return newStats;
+        });
+      });
+
+      if (Answer === question.correctAnswer) {
+        setAnswerStreak(answerStreak + 1);
+        setCorrectAnswers(correctAnswers + 1);
+        if (answerStreak + 1 > bestAnswerStreak) {
+          setBestAnswerStreak(answerStreak + 1);
+        }
+      } else {
+        setAnswerStreak(0);
+        setIncorrectAnswers(incorrectAnswers + 1);
+      }
+
+      fetchQuestion();
+      setUserAnswer(null);
+      setTimer(15);
+    }, 3000);
+  }
+};
+
+
+
 
   return (
     <div className="App">
@@ -110,13 +153,13 @@ function App() {
           TriviaTempo
         </div>
         <div className="timer">
-        {timer}
+          {timer}
         </div>
         <div className="question-container">
           {question && (
             <>
               <h2>{question.question.text}</h2>
-              <h4> {question.tags.join(', ')} </h4>
+              <h4>{question.tags.join(', ')}</h4>
               <ul>
                 {question.answers?.map(answer => (
                   <button
@@ -138,46 +181,51 @@ function App() {
           )}
         </div>
         <div className="buttons">
-       <Button style={{ color: 'white', fontFamily: 'Lato', border: '1px solid white',}} onClick={() => setView('basic')}> Answer Stats </Button>
-<Button style={{ color: 'white', fontFamily: 'Lato', border: '1px solid white', }} onClick={() => setView('stats')}> Type Stats </Button>
-
+          <Button style={{ color: 'white', fontFamily: 'Lato', border: '1px solid white', }} onClick={() => setView('basic')}> Answer Stats </Button>
+          <Button style={{ color: 'white', fontFamily: 'Lato', border: '1px solid white', }} onClick={() => setView('stats')}> Type Stats </Button>
         </div>
-        {view=='basic' && (
-        <span className="stats">
-          <p> Correct Answers: {correctAnswers} </p>
-          <p> Incorrect Answers: {incorrectAnswers} </p>
-          <p> Current Answer Streak: {answerStreak} </p>
-          <p> Best Answer Streak: {bestAnswerStreak} </p>
-        </span>
-        )
-        }
-  {view === 'stats' && (
-  <div>
-  <div className="buttons2">
-       <Button style={{ color: 'white', fontFamily: 'Lato', border: '1px solid white',}} onClick={() => setStatsView('total')}> Total Stats </Button>
-<Button style={{ color: 'white', fontFamily: 'Lato', border: '1px solid white', }} onClick={() => setStatsView('percentage')}> Percentage Stats </Button>
-
+        {view === 'basic' && (
+          <span className="stats">
+            <p> Correct Answers: {correctAnswers} </p>
+            <p> Incorrect Answers: {incorrectAnswers} </p>
+            <p> Current Answer Streak: {answerStreak} </p>
+            <p> Best Answer Streak: {bestAnswerStreak} </p>
+          </span>
+        )}
+        {view === 'stats' && (
+          <div>
+            <div className="buttons2">
+              <Button style={{ color: 'white', fontFamily: 'Lato', border: '1px solid white', }} onClick={() => setStatsView('total')}> Total Stats </Button>
+             
+            </div>
+            {statsView === 'total' && (
+              <div>
+                {questionTags.length > 0 && (
+                  <div className="stats">
+                    {Array.from(new Set(questionTags))
+                      .sort((a, b) => questionTags.filter(t => t === b).length - questionTags.filter(t => t === a).length)
+                      .map(tag => (
+                        <p key={tag}>{`${tag}: ${questionTags.filter(t => t === tag).length}`}</p>
+                      ))}
+                  </div>
+                )}
+              </div>
+            )}
+ {statsView === 'percentage' && (
+        <div className="stats">
+         
+          {Object.entries(tagStats)
+            .map(([tag, stats]) => (
+              <p key={tag}>
+                {`${tag}: ${(((stats.correct )/ ((stats.total))) * 100).toFixed(2)}% (Correct: ${stats.correct}, Incorrect: ${stats.total - stats.correct}, Total: ${stats.total})`}
+              </p>
+            ))
+            .sort((a, b) => b.percentage - a.percentage)}
         </div>
-   {statsView === 'total' && (
-   <div>
-    {questionTags.length > 0 && (
-      <div className="stats">
-        {Array.from(new Set(questionTags))
-          .sort((a, b) => questionTags.filter(t => t === b).length - questionTags.filter(t => t === a).length)
-          .map(tag => (
-            <p key={tag}>{`${tag}: ${questionTags.filter(t => t === tag).length}`}</p>
-          ))}
-      </div>
-    )}
-    </div>
-    )}
-       {statsView === 'percentage' && (
-       <div>
+      )}
 
-       </div>
-       )}
-  </div>
-)}
+          </div>
+        )}
       </header>
     </div>
   );
